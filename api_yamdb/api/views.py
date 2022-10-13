ecaad0d4bffe3bd61d84ceaf1b96bf2b7f2da141
VankoID <dudarev.id@gmail.com>
2022-10-13 16:34:12 +0400

    Update2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -139,8 +139,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
-        title_queryset = title.reviews.all()
-        return title_queryset
+        return title.reviews.all()
 
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
@@ -156,8 +155,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         review_id = self.kwargs.get('review_id')
         review = get_object_or_404(Review, pk=review_id)
-        review_queryset = review.comments.all()
-        return review_queryset
+        return review.comments.all()
 
     def perform_create(self, serializer):
         review_id = self.kwargs.get('review_id')
